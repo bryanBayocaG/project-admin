aca28a5e2fb3e92c809cb76a4db87a8b7ab06fd0
Bryan Bayoca <bryanbayoca686@gmail.com>
2025-05-14 22:37:35 +0800

    login api

---
+++ laravel-admin/app/Http/Controllers/AuthController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\RegisterRequest;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Symfony\Component\HttpFoundation\Response;
+
+class AuthController extends Controller
+{
+    //
+    public function register(RegisterRequest $request){
+        $user = User::create([
+            'firstName' => $request->input('firstName'),
+            'lastName' => $request->input('lastName'),
+            'email' => $request->input('email'),
+            'password' => Hash::make($request->input('password')),
+
+        ]);
+        return response($user, Response::HTTP_CREATED);
+    }
+
+    public function login(Request $request){
+        if(!Auth::attempt($request->only('email', 'password'))){
+            return response([
+                'error' => 'Invalid Credentials'
+            ], Response::HTTP_UNAUTHORIZED);
+        }
+        /** @var User $user */
+        $user = Auth::user();
+        $token = $user->createToken('token')->plainTextToken;
+        return response([
+            'jwt' => $token
+        ]);
+    }
+}
