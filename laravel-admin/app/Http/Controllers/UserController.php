88cbf28ba831681486bdcee0a34f414551f59201
Bryan Bayoca <bryanbayoca686@gmail.com>
2025-05-16 23:45:05 +0800

    without data wrapper

--- laravel-admin/app/Http/Controllers/UserController.php
+++ laravel-admin/app/Http/Controllers/UserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\UserCreateRequest;
 use App\Http\Requests\UserUpdateRequest;
+use App\Http\Resources\UserResource;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
@@ -13,7 +14,7 @@ class UserController extends Controller
 {
     public function index()
     {
-        return User::with('role')->paginate();
+        return UserResource::collection(User::paginate());
     }
   
     public function store(UserCreateRequest $request)
@@ -26,7 +27,7 @@ class UserController extends Controller
  
     public function show(string $id)
     {
-        return User::with('role')->find($id);
+        return new UserResource(User::find($id));
     }
 
     public function update(UserUpdateRequest $request, string $id)
